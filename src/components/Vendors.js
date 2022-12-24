e4f4331d3bad67ab7ce419e630e9fb3c041d6665
cjustinobi <cjustinobi@gmail.com>
2022-12-24 17:44:24 +0100

    remove unused modal

--- src/components/Vendors.js
+++ src/components/Vendors.js
@@ -39,7 +39,7 @@ const Vendors = () => {
 
     vendorsHandler()
 
-  }, [getVendors, updateVendor])
+  }, [getVendors, updateVendor, setUpdateVendor])
 
   return (
     <div className="product-container">
