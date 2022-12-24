0234dccb1569e21d1fd2a8b91e2461be6a785ddf
cjustinobi <cjustinobi@gmail.com>
2022-12-24 21:50:00 +0100

    remove unused modal

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -8,9 +8,9 @@ const Header = ({ openVendorModal }) => {
   return (
     <header>
 
-      <div class="header-main">
+      <div className="header-main">
 
-        <div class="container">
+        <div className="container">
 
           <Link to='/' className="header-logo">
             <img src={logo} alt="" width="100px" />
@@ -21,7 +21,7 @@ const Header = ({ openVendorModal }) => {
             <Link to='/my-transactions'>My Transactions</Link>
           </div>
 
-          <div class="header-user-actions">
+          <div className="header-user-actions">
 
             {/*<button className={'action-btn'} onClick={() => openTransactionModal()} >create tx</button>*/}
             <button className={'banner-btn'} onClick={() => openVendorModal()} >Create Account</button>
@@ -31,7 +31,7 @@ const Header = ({ openVendorModal }) => {
 
       </div>
 
-      <div class="mobile-bottom-navigation">
+      <div className="mobile-bottom-navigation">
 
         <button onClick={() => navigate('/')} className="action-btn">
           <ion-icon name="home-outline"></ion-icon>
@@ -47,104 +47,6 @@ const Header = ({ openVendorModal }) => {
         </button>
 
       </div>
-
-      <nav class="mobile-navigation-menu  has-scrollbar" data-mobile-menu>
-
-        <div class="menu-top">
-          <h2 class="menu-title">Menu</h2>
-
-          <button className="menu-close-btn" data-mobile-menu-close-btn>
-            <ion-icon name="close-outline"></ion-icon>
-          </button>
-        </div>
-
-        <ul class="mobile-menu-category-list">
-
-          <li className="menu-category">
-            <a href="#" className="menu-title">Home</a>
-          </li>
-        </ul>
-
-        <div class="menu-bottom">
-
-          <ul class="menu-category-list">
-
-            <li className="menu-category">
-
-              <button className="accordion-menu" data-accordion-btn>
-                <p class="menu-title">Language</p>
-
-                <ion-icon name="caret-back-outline" className="caret-back"></ion-icon>
-              </button>
-
-              <ul class="submenu-category-list" data-accordion>
-
-                <li className="submenu-category">
-                  <a href="#" className="submenu-title">English</a>
-                </li>
-
-                <li className="submenu-category">
-                  <a href="#" className="submenu-title">Espa&ntilde;ol</a>
-                </li>
-
-                <li className="submenu-category">
-                  <a href="#" className="submenu-title">Fren&ccedil;h</a>
-                </li>
-
-              </ul>
-
-            </li>
-
-            <li className="menu-category">
-              <button className="accordion-menu" data-accordion-btn>
-                <p class="menu-title">Currency</p>
-                <ion-icon name="caret-back-outline" className="caret-back"></ion-icon>
-              </button>
-
-              <ul class="submenu-category-list" data-accordion>
-                <li className="submenu-category">
-                  <a href="#" className="submenu-title">USD &dollar;</a>
-                </li>
-
-                <li className="submenu-category">
-                  <a href="#" className="submenu-title">EUR &euro;</a>
-                </li>
-              </ul>
-            </li>
-
-          </ul>
-
-          <ul class="menu-social-container">
-
-            <li>
-              <a href="#" className="social-link">
-                <ion-icon name="logo-facebook"></ion-icon>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="social-link">
-                <ion-icon name="logo-twitter"></ion-icon>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="social-link">
-                <ion-icon name="logo-instagram"></ion-icon>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="social-link">
-                <ion-icon name="logo-linkedin"></ion-icon>
-              </a>
-            </li>
-
-          </ul>
-
-        </div>
-
-      </nav>
 
     </header>
   )
